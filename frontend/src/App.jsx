0f2403c2e442dd2a19590f237dd23a051fa6f6e0
JayaStack <jayasarathy.dev@gmail.com>
2026-02-03 18:57:54 +0530

    Phase 9.2 : First Real UI - Anime Grid

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import api from "./api/axios";
 
 function App() {
+  const [anime, setAnime] = useState([]);
+
   useEffect(() => {
     api.get("/anime").then((res) => {
-      console.log("Anime from backend:", res.data);
+      setAnime(res.data.data);
     });
   }, []);
 
-  return <h1>MyAnimeList Frontend</h1>;
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>MyAnimeList</h1>
+
+      {anime.map((a) => (
+        <div key={a._id} style={{ marginBottom: 10 }}>
+          <h3>{a.title}</h3>
+          <p>{a.description}</p>
+          <small>
+            Rating: {a.averageRating} | Episodes: {a.episodes}
+          </small>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
